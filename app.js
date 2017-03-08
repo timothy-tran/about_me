eb889440fb7c49fcb756e47704b6adb9e3461a6b
Timothy Tran <timotran@gmail.com>
2017-03-08 03:59:49 -0800

    Add if else statements to app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict'
+
+var question1 = 'String1';
+var question2 = 'string2';
+var question3 = 'string3';
+var question4 = 'string4';
+var question5 = 'string5';
+var question;
+
+for (var i = 1, i =<5, i++) {
+  if (i === 1) {
+    question = question1;
+  } else if (i === 2) {
+    question = question2;
+  } else if (i === 3) {
+    question = question3;
+  } else if (i === 4) {
+    question = question4;
+  } else {
+    question = question5;
+  }
+  
+}
