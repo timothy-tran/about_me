bfd1ab5e2d33e0a4a84407ecb7e9ea318854ab7c
Timothy Tran <timotran@gmail.com>
2017-03-08 05:54:06 -0800

    Revise the code in app.js and debugg

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
 'use strict'
 
-var question1 = 'String1';
-var question2 = 'string2';
-var question3 = 'string3';
-var question4 = 'string4';
-var question5 = 'string5';
+var question1 = 'Do I play music instrucment?';
+var question2 = 'Can I un a marathon?';
+var question3 = 'Do I like fastfood?';
+var question4 = 'Do I like to travel?';
+var question5 = 'Can I write computer code?';
 var question;
 var userAnswer;
 var count = 1;
 
-for (var i = 1, i =<5, i++) {
-  if (i === 1) {
-    question = question1;
-  } else if (i === 2) {
-    question = question2;
-  } else if (i === 3) {
-    question = question3;
-  } else if (i === 4) {
-    question = question4;
+userAnswer = prompt(question1);
+var myAnswer = userAnswer.toLowerCase();
+if (myAnswer === 'yes' || myAnswer === 'y') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("You are right. I play flute!")
+  console.log(myAnswer);
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("Mmh...only you don\'t play music.")
+  console.log(userAnswer);
   } else {
-    question = question5;
-  }
-  userAnswer = prompt(question);
-  myAnswer = userAnswer.toLowCase();
-  if (myAnswer === 'yes') || (myAnswer === 'y') {
-    var bool = confirm('Your answer is' + ' ' + myAnswer);
-    if (bool) {
-      alert('Bravo! You\'re correct!')
-      count = count++;
-    } else {
-      alert('I\'m sorry. I\'s not right')
-    }
-  }
+  alert("Sorry, you didnot enter right answer");
+}
+
+userAnswer = prompt(question2);
+var myAnswer = userAnswer.toLowerCase();
+if (myAnswer === 'yes' || myAnswer === 'y') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("You are right. I play flute!")
+  console.log(myAnswer);
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("Mmh...only you don\'t play music.")
+  console.log(userAnswer);
+  } else {
+  alert("Sorry, you didnot enter right answer");
+}
+
+userAnswer = prompt(question3);
+var myAnswer = userAnswer.toLowerCase();
+if (myAnswer === 'yes' || myAnswer === 'y') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("You are right. I play flute!")
+  console.log(myAnswer);
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("Mmh...only you don\'t play music.")
+  console.log(userAnswer);
+  } else {
+  alert("Sorry, you didnot enter right answer");
+}
+
+userAnswer = prompt(question4);
+var myAnswer = userAnswer.toLowerCase();
+if (myAnswer === 'yes' || myAnswer === 'y') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("You are right. I play flute!")
+  console.log(myAnswer);
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("Mmh...only you don\'t play music.")
+  console.log(userAnswer);
+  } else {
+  alert("Sorry, you didnot enter right answer");
+}
+
+userAnswer = prompt(question5);
+var myAnswer = userAnswer.toLowerCase();
+if (myAnswer === 'yes' || myAnswer === 'y') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("You are right. I play flute!")
+  console.log(myAnswer);
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+  confirm("Your answer is" + " " + myAnswer);
+  alert("Mmh...only you don\'t play music.")
+  console.log(userAnswer);
+  } else {
+  alert("Sorry, you didnot enter right answer");
 }
