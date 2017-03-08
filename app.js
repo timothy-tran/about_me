b7db486b8431cf32ebdbee7e0be69a1affc4c99a
Timothy Tran <timotran@gmail.com>
2017-03-08 04:21:13 -0800

    Add alert and confirm to app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var question3 = 'string3';
 var question4 = 'string4';
 var question5 = 'string5';
 var question;
+var userAnswer;
+var count = 1;
 
 for (var i = 1, i =<5, i++) {
   if (i === 1) {
@@ -19,5 +21,15 @@ for (var i = 1, i =<5, i++) {
   } else {
     question = question5;
   }
-  
+  userAnswer = prompt(question);
+  myAnswer = userAnswer.toLowCase();
+  if (myAnswer === 'yes') || (myAnswer === 'y') {
+    var bool = confirm('Your answer is' + ' ' + myAnswer);
+    if (bool) {
+      alert('Bravo! You\'re correct!')
+      count = count++;
+    } else {
+      alert('I\'m sorry. I\'s not right')
+    }
+  }
 }
