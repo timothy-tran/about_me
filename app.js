651b0beb6950af8f625259f4b4eb9130adabd8d1
Anthony Robinson <robinsonamk@gmail.com>
2017-03-09 15:58:35 -0800

    update to code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var question1 = 'Do I play music instrucment?';
-var question2 = 'Can I un a marathon?';
+var question1 = 'Do I play a musical instrument?';
+var question2 = 'Can I run a marathon?';
 var question3 = 'Do I like fastfood?';
 var question4 = 'Do I like to travel?';
 var question5 = 'Can I write computer code?';
@@ -26,12 +26,12 @@ function myQuestion1() {
   var b = isNaN(answer);
   if (!b) {
     if (answer) {
-      confirm('Your answer is' + '' + answer);
+      confirm('Your answer is' + ' ' + userInput);
       alert('you are right. I play flute!');
       correct++;
       console.log(answer);
     } else {
-      confirm('your answer is' + '' + answer);
+      confirm('your answer is' + ' ' + userInput);
       alert('Mmh...only you don\'t play music.');
     }
   } else {
@@ -41,76 +41,86 @@ function myQuestion1() {
 
 myQuestion1();
 
-//
-// userAnswer = prompt(question1);
-// var myAnswer = userAnswer.toLowerCase();
-// if (myAnswer === 'yes' || myAnswer === 'y') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("You are right. I play flute!");
-//   console.log(myAnswer);
-// } else if (myAnswer === 'no' || myAnswer === 'n') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Mmh...only you don\'t play music.");
-//   console.log(userAnswer);
-// } else {
-//   alert("Sorry, you didn\'t enter right answer");
-// }
-//
-// userAnswer = prompt(question2);
-// var myAnswer = userAnswer.toLowerCase();
-// if (myAnswer === 'yes' || myAnswer === 'y') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Are you crazy. Who would run a marathon!")
-//   console.log(myAnswer);
-// } else if (myAnswer === 'no' || myAnswer === 'n') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("I think you\'re right this time.");
-//   console.log(userAnswer);
-// } else {
-//   alert("Sorry, you didn\'t enter right answer");
-// }
-//
-// userAnswer = prompt(question3);
-// var myAnswer = userAnswer.toLowerCase();
-// if (myAnswer === 'yes' || myAnswer === 'y') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("You are right. Who wouln\'t like fastfood.")
-//   console.log(myAnswer);
-// } else if (myAnswer === 'no' || myAnswer === 'n') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Mmh...I am not that health conscious.");
-//   console.log(userAnswer);
-// } else {
-//   alert("Sorry, you didn\'t enter right answer");
-// }
-//
-// userAnswer = prompt(question4);
-// var myAnswer = userAnswer.toLowerCase();
-// if (myAnswer === 'yes' || myAnswer === 'y') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Mmh...I sit at computer desk all day. So you\'re wrong!")
-//   console.log(myAnswer);
-// } else if (myAnswer === 'no' || myAnswer === 'n') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Yes...you\'re right.");
-//   console.log(userAnswer);
-// } else {
-//   alert("Sorry, you didn\'t enter right answer");
-// }
-//
-// userAnswer = prompt(question5);
-// var myAnswer = userAnswer.toLowerCase();
-// if (myAnswer === 'yes' || myAnswer === 'y') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("You are right. I am a computer geek!");
-//   console.log(myAnswer);
-// } else if (myAnswer === 'no' || myAnswer === 'n') {
-//   confirm("Your answer is" + " " + myAnswer);
-//   alert("Mmh...think again.");
-//   console.log(userAnswer);
-// } else {
-//   alert("Sorry, you didn\'t enter right answer");
-// }
-// }
-//
-// function myanswer()
+function myQuestion2() {
+  var userInput = prompt(question2);
+  var answer = yesorno(userInput);
+  var b = isNaN(answer);
+  if (!b) {
+    if (answer) {
+      confirm('Your answer is' + ' ' + userInputr);
+      alert('Are you crazy. Who would run a marathon!');
+      correct++;
+      console.log(answer);
+    } else {
+      confirm('your answer is' + ' ' + userInput);
+      alert('I think you\'re right this time.');
+    }
+  } else {
+    alert(answer);
+  }
+}
+
+myQuestion2();
+
+function myQuestion3() {
+  var userInput = prompt(question3);
+  var answer = yesorno(userInput);
+  var b = isNaN(answer);
+  if (!b) {
+    if (answer) {
+      confirm('Your answer is' + ' ' + userInput);
+      alert('You are right. Who wouln\'t like fastfood.');
+      correct++;
+      console.log(answer);
+    } else {
+      confirm('your answer is' + ' ' + userInput);
+      alert('Mmh...I am not that health conscious.');
+    }
+  } else {
+    alert(answer);
+  }
+}
+
+myQuestion3();
+
+function myQuestion4() {
+  var userInput = prompt(question2);
+  var answer = yesorno(userInput);
+  var b = isNaN(answer);
+  if (!b) {
+    if (answer) {
+      confirm('Your answer is' + '' + answer);
+      alert('Mmh...I sit at computer desk all day. So you\'re wrong!');
+      correct++;
+      console.log(answer);
+    } else {
+      confirm('your answer is' + '' + answer);
+      alert('Sorry, you didn\'t enter right answer');
+    }
+  } else {
+    alert(answer);
+  }
+}
+
+myQuestion4();
+
+function myQuestion5() {
+  var userInput = prompt(question2);
+  var answer = yesorno(userInput);
+  var b = isNaN(answer);
+  if (!b) {
+    if (answer) {
+      confirm('Your answer is' + '' + answer);
+      alert('You are right. I am a computer geek!');
+      correct++;
+      console.log(answer);
+    } else {
+      confirm('your answer is' + '' + answer);
+      alert('Mmh...think again.');
+    }
+  } else {
+    alert(answer);
+  }
+}
+
+myQuestion5();
