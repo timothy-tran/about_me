202518dabf84a352b74bf54509cdb7e59d285175
Anthony Robinson <robinsonamk@gmail.com>
2017-03-09 15:18:00 -0800

    first wuestion done!

--- app.js
+++ app.js
@@ -7,74 +7,110 @@ var question4 = 'Do I like to travel?';
 var question5 = 'Can I write computer code?';
 var question;
 var userAnswer;
-var count = 1;
+var correct = 0;
 
-userAnswer = prompt(question1);
-var myAnswer = userAnswer.toLowerCase();
-if (myAnswer === 'yes' || myAnswer === 'y') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("You are right. I play flute!");
-  console.log(myAnswer);
-} else if (myAnswer === 'no' || myAnswer === 'n') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Mmh...only you don\'t play music.");
-  console.log(userAnswer);
-} else {
-  alert("Sorry, you didn\'t enter right answer");
+function yesorno(userAnswer) {
+  var myAnswer = userAnswer.toLowerCase();
+  if (myAnswer === 'yes' || myAnswer === 'y') {
+    return true;
+  } else if (myAnswer === 'no' || myAnswer === 'n') {
+    return false;
+  } else {
+    return 'sorry, you didn\'t enter the right answer';
+  }
 }
 
-userAnswer = prompt(question2);
-var myAnswer = userAnswer.toLowerCase();
-if (myAnswer === 'yes' || myAnswer === 'y') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Are you crazy. Who would run a marathon!")
-  console.log(myAnswer);
-} else if (myAnswer === 'no' || myAnswer === 'n') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("I think you\'re right this time.");
-  console.log(userAnswer);
-} else {
-  alert("Sorry, you didn\'t enter right answer");
+function myQuestion1() {
+  var userInput = prompt(question1);
+  var answer = yesorno(userInput);
+  var b = isNaN(answer);
+  if (!b) {
+    if (answer) {
+      confirm('Your answer is' + '' + answer);
+      alert('you are right. I play flute!');
+      correct++;
+      console.log(answer);
+    } else {
+      confirm('your answer is' + '' + answer);
+      alert('Mmh...only you don\'t play music.');
+    }
+  } else {
+    alert(answer);
+  }
 }
 
-userAnswer = prompt(question3);
-var myAnswer = userAnswer.toLowerCase();
-if (myAnswer === 'yes' || myAnswer === 'y') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("You are right. Who wouln\'t like fastfood.")
-  console.log(myAnswer);
-} else if (myAnswer === 'no' || myAnswer === 'n') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Mmh...I am not that health conscious.");
-  console.log(userAnswer);
-} else {
-  alert("Sorry, you didn\'t enter right answer");
-}
+myQuestion1();
 
-userAnswer = prompt(question4);
-var myAnswer = userAnswer.toLowerCase();
-if (myAnswer === 'yes' || myAnswer === 'y') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Mmh...I sit at computer desk all day. So you\'re wrong!")
-  console.log(myAnswer);
-} else if (myAnswer === 'no' || myAnswer === 'n') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Yes...you\'re right.");
-  console.log(userAnswer);
-} else {
-  alert("Sorry, you didn\'t enter right answer");
-}
-
-userAnswer = prompt(question5);
-var myAnswer = userAnswer.toLowerCase();
-if (myAnswer === 'yes' || myAnswer === 'y') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("You are right. I am a computer geek!");
-  console.log(myAnswer);
-} else if (myAnswer === 'no' || myAnswer === 'n') {
-  confirm("Your answer is" + " " + myAnswer);
-  alert("Mmh...think again.");
-  console.log(userAnswer);
-} else {
-  alert("Sorry, you didn\'t enter right answer");
-}
+//
+// userAnswer = prompt(question1);
+// var myAnswer = userAnswer.toLowerCase();
+// if (myAnswer === 'yes' || myAnswer === 'y') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("You are right. I play flute!");
+//   console.log(myAnswer);
+// } else if (myAnswer === 'no' || myAnswer === 'n') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Mmh...only you don\'t play music.");
+//   console.log(userAnswer);
+// } else {
+//   alert("Sorry, you didn\'t enter right answer");
+// }
+//
+// userAnswer = prompt(question2);
+// var myAnswer = userAnswer.toLowerCase();
+// if (myAnswer === 'yes' || myAnswer === 'y') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Are you crazy. Who would run a marathon!")
+//   console.log(myAnswer);
+// } else if (myAnswer === 'no' || myAnswer === 'n') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("I think you\'re right this time.");
+//   console.log(userAnswer);
+// } else {
+//   alert("Sorry, you didn\'t enter right answer");
+// }
+//
+// userAnswer = prompt(question3);
+// var myAnswer = userAnswer.toLowerCase();
+// if (myAnswer === 'yes' || myAnswer === 'y') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("You are right. Who wouln\'t like fastfood.")
+//   console.log(myAnswer);
+// } else if (myAnswer === 'no' || myAnswer === 'n') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Mmh...I am not that health conscious.");
+//   console.log(userAnswer);
+// } else {
+//   alert("Sorry, you didn\'t enter right answer");
+// }
+//
+// userAnswer = prompt(question4);
+// var myAnswer = userAnswer.toLowerCase();
+// if (myAnswer === 'yes' || myAnswer === 'y') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Mmh...I sit at computer desk all day. So you\'re wrong!")
+//   console.log(myAnswer);
+// } else if (myAnswer === 'no' || myAnswer === 'n') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Yes...you\'re right.");
+//   console.log(userAnswer);
+// } else {
+//   alert("Sorry, you didn\'t enter right answer");
+// }
+//
+// userAnswer = prompt(question5);
+// var myAnswer = userAnswer.toLowerCase();
+// if (myAnswer === 'yes' || myAnswer === 'y') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("You are right. I am a computer geek!");
+//   console.log(myAnswer);
+// } else if (myAnswer === 'no' || myAnswer === 'n') {
+//   confirm("Your answer is" + " " + myAnswer);
+//   alert("Mmh...think again.");
+//   console.log(userAnswer);
+// } else {
+//   alert("Sorry, you didn\'t enter right answer");
+// }
+// }
+//
+// function myanswer()
