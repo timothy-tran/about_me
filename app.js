944e054d8ee90c882fd25c5d19a421c3ea9af337
Timothy Tran <timotran@gmail.com>
2017-03-12 01:37:21 -0800

    Add the seven question for app.js

--- app.js
+++ app.js
@@ -1,139 +1,21 @@
 'use strict';
 
-var question3 = ;
-var question4 = ;
-var question5 = ;
+var question1 = 'Do I play a musical instrument?';
+var question2 = 'Can I run a marathon?';
+var question3 = 'Do I like fastfood?';
+var question4 = 'Do I like to travel?';
+var question5 = 'Can I write computer code?';
 var question;
-
-var userAnswer;
-var correct = 0;
-
-function yesOrNo(userAnswer) {
-  var myAnswer = userAnswer.toLowerCase();
-  if (myAnswer === 'yes' || myAnswer === 'y') {
-    return true;
-  } else if (myAnswer === 'no' || myAnswer === 'n') {
-    return false;
-  } else {
-    return 'Sorry, you didn\'t enter the right answer!';
-  }
-}
-
-function myQuestion1() {
-  var userInput = prompt('Do I play a musical instrument?');
-  var answer = yesOrNo(userInput);
-  var checkIfString = isNaN(answer);
-  if (!checkIfString) {
-    if (answer) {
-      confirm('Your answer is' + ' ' + userInput);
-      alert('you are right. I play flute!');
-      correct++;
-      console.log(answer);
-    } else {
-      confirm('Your answer is' + ' ' + userInput);
-      alert('Mmh...only you don\'t play music.');
-    }
-  } else {
-    alert(answer);
-  }
-}
-
-function myQuestion2() {
-  var userInput = prompt('Can I run a marathon?');
-  var answer = yesorno(userInput);
-  var checkIfString = isNaN(answer);
-  if (!checkIfString) {
-    if (answer) {
-      confirm('Your answer is' + ' ' + userInput);
-      alert('Are you crazy. Who would run a marathon!');
-      correct++;
-      console.log(answer);
-    } else {
-      confirm('your answer is' + ' ' + userInput);
-      alert('I think you\'re right this time.');
-    }
-  } else {
-    alert(answer);
-  }
-}
-
-function myQuestion3() {
-  var userInput = prompt('Do I like fastfood?');
-  var answer = yesorno(userInput);
-  var checkIfString = isNaN(answer);
-  if (!checkIfString) {
-    if (answer) {
-      confirm('Your answer is' + ' ' + userInput);
-      alert('You are right. Who wouln\'t like fastfood.');
-      correct++;
-      console.log(answer);
-    } else {
-      confirm('your answer is' + ' ' + userInput);
-      alert('Mmh...I am not that health conscious.');
-    }
-  } else {
-    alert(answer);
-  }
-}
-
-function myQuestion4() {
-  var userInput = prompt('Do I like to travel?');
-  var answer = yesorno(userInput);
-  var checkIfString = isNaN(answer);
-  if (!checkIfString) {
-    if (answer) {
-      confirm('Your answer is' + '' + answer);
-      alert('Mmh...I sit at computer desk all day. So you\'re wrong!');
-      correct++;
-      console.log(answer);
-    } else {
-      confirm('your answer is' + '' + answer);
-      alert('Sorry, you didn\'t enter right answer');
-    }
-  } else {
-    alert(answer);
-  }
-}
-
-function myQuestion5() {
-  var userInput = prompt('Can I write computer code?');
-  var answer = yesorno(userInput);
-  var checkIfString = isNaN(answer);
-  if (!checkIfString) {
-    if (answer) {
-      confirm('Your answer is' + '' + answer);
-      alert('You are right. I am a computer geek!');
-      correct++;
-      console.log(answer);
-    } else {
-      confirm('your answer is' + '' + answer);
-      alert('Mmh...think again.');
-    }
-  } else {
-    alert(answer);
-  }
-}
-
-myQuestion1();
-myQuestion2();
-myQuestion3();
-myQuestion4();
-myQuestion5();
-=======
-var correct = 0;
-var notCorrect = 0;
 
 var userAnswer = prompt(question1);
 var myAnswer = userAnswer.toLowerCase();
 if (myAnswer === 'yes' || myAnswer === 'y') {
   confirm("Your answer is" + " " + myAnswer);
   alert("You are right. I play flute!");
-  correct++;
   console.log(myAnswer);
 } else if (myAnswer === 'no' || myAnswer === 'n') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Mmh...only you don\'t play music.");
-  notCorrect++;
   console.log(userAnswer);
 } else {
   alert("Sorry, you didn\'t enter right answer");
@@ -144,12 +26,10 @@ var myAnswer = userAnswer.toLowerCase();
 if (myAnswer === 'yes' || myAnswer === 'y') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Are you crazy. Who would run a marathon!");
-  notCorrect++;
   console.log(myAnswer);
 } else if (myAnswer === 'no' || myAnswer === 'n') {
   confirm("Your answer is" + " " + myAnswer);
   alert("I think you\'re right this time.");
-  correct++;
   console.log(userAnswer);
 } else {
   alert("Sorry, you didn\'t enter right answer");
@@ -160,12 +40,10 @@ var myAnswer = userAnswer.toLowerCase();
 if (myAnswer === 'yes' || myAnswer === 'y') {
   confirm("Your answer is" + " " + myAnswer);
   alert("You are right. Who wouln\'t like fastfood.");
-  correct++;
   console.log(myAnswer);
 } else if (myAnswer === 'no' || myAnswer === 'n') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Mmh...I am not that health conscious.");
-  notCorrect++;
   console.log(userAnswer);
 } else {
   alert("Sorry, you didn\'t enter right answer");
@@ -176,12 +54,10 @@ var myAnswer = userAnswer.toLowerCase();
 if (myAnswer === 'yes' || myAnswer === 'y') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Mmh...I sit at computer desk all day. So you\'re wrong!");
-  notCorrect++;
   console.log(myAnswer);
 } else if (myAnswer === 'no' || myAnswer === 'n') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Yes...you\'re right.");
-  correct++;
   console.log(userAnswer);
 } else {
   alert("Sorry, you didn\'t enter right answer");
@@ -192,12 +68,10 @@ var myAnswer = userAnswer.toLowerCase();
 if (myAnswer === 'yes' || myAnswer === 'y') {
   confirm("Your answer is" + " " + myAnswer);
   alert("You are right. I am a computer geek!");
-  correct++;
   console.log(myAnswer);
 } else if (myAnswer === 'no' || myAnswer === 'n') {
   confirm("Your answer is" + " " + myAnswer);
   alert("Mmh...think again.");
-  notCorrect++;
   console.log(userAnswer);
 } else {
   alert("Sorry, you didn\'t enter right answer");
@@ -215,16 +89,29 @@ for (var i = 0; i < 4; i++) {
     console.log("Test user input:" + myNum);
   if (myNum < numTarget) {
     alert("Your guessing number is too low. Try again!");
-    notCorrect++;
-    console.log("Your guess number:" + " " + myNum + " " + "is too low.");
+    console.log("Your guess number:" + " " + myNum + " " + " is too low.");
   } else if (myNum > numTarget) {
     alert("Your guessing number is too high. Try again!");
-    notCorrect++;
-    console.log("Your guess number:" + " " + myNum + " " + "is too high.");
+    console.log("Your guess number:" + " " + myNum + " " + " is too high.");
   } else {
     alert("You\'re right on target.");
     i = 4;
-    correct++;
     console.log(myNum);
+  }
+}
+
+var stateVisited = ['CALIFORNIA', 'OREGON', 'ARIZON'];
+var stateShortForm = ['CA', 'OR', 'AZ'];
+for (var i = 1; i <= 6; i++) {
+  var userInput = prompt('Can you guess what state I have visited besides Washington?');
+  var myInput = userInput.toUpperCase();
+  console.log(myInput);
+  if (stateVisited.includes(myInput) ||     stateShortForm.includes(myInput)) {
+    alert('You are correct. I have been to ' + myInput);
+    break;
+  } else if (i >= 6) {
+    alert('Sorry, you didn\'t guess right, but your try is run out!');
+  } else {
+    alert('Sorry, you didn\'t enter the right answer! Please try again. Try number: ' + i);
   }
 }
